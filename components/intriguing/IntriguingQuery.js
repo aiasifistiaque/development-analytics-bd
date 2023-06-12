dd5a90d85f76c142a2baff89edd1e8d51e201667
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2023-06-12 11:27:14 +0600

    home page done

---
+++ components/intriguing/IntriguingQuery.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Center, Flex, Text } from "@chakra-ui/react";
+const IntriguingQuery = () => {
+  return (
+    <Flex
+      background="linear-gradient(90deg, #9347FF 24.79%, #25BBD4 100%)"
+      justify="center"
+      align="center"
+      color="white"
+      height="485px"
+    >
+      <Center
+        flexDir="column"
+        maxW="450px"
+        gap="26px"
+        mx={{ base: "16px", lg: "128px" }}
+      >
+        <Text fontWeight="900" fontSize="32px">
+          Sound intriguiging?
+        </Text>
+        <Text fontSize="16px" fontWeight="500" textAlign="center">
+          CONTACT US TODAY TO KNOW MORE ABOUT OUR SERVICES AND CONNECT!
+        </Text>
+        <Button
+          border="2px solid white"
+          borderRadius="none"
+          background="transparent"
+          color="white"
+          fontWeight="700"
+          fontSize="16px"
+          padding="24px 36px"
+        >
+          CONTACT US
+        </Button>
+      </Center>
+    </Flex>
+  );
+};
+
+export default IntriguingQuery;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Prompt from "@/components/prompt/Prompt";
 import Specialization from "@/components/specialization/Specialization";
 import Personnel from "@/components/personnel/Personnel";
 import ServiceDetails from "@/components/service-details/ServiceDetails";
+import IntriguingQuery from "@/components/intriguing/IntriguingQuery";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function Home() {
       <Specialization />
       <Personnel />
       <ServiceDetails />
+      <IntriguingQuery />
     </Page>
   );
 }
